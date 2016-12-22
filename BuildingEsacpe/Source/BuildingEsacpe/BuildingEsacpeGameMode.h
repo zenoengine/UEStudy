81eb31b76c26b683cd902a93983fd32af485df22
ZenoAhn <zenoengine@gmail.com>
2016-12-22 13:29:23 +0900

    initialized project added .girignore,

---
+++ BuildingEsacpe/Source/BuildingEsacpe/BuildingEsacpeGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "BuildingEsacpeGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDINGESACPE_API ABuildingEsacpeGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
