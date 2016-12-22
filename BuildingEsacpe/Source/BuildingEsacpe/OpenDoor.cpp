e0cc3c6546262c26d2e12372b0d1ff42ad0bcb80
ZenoAhn <zenoengine@gmail.com>
2016-12-22 20:01:07 +0900

    maked level added OpenDoor component

---
+++ BuildingEsacpe/Source/BuildingEsacpe/OpenDoor.cpp
@@ -0,0 +1,39 @@
+// Copyright Zeno Ahn 2016.
+
+#include "BuildingEsacpe.h"
+#include "OpenDoor.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	bWantsBeginPlay = true;
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	AActor* Owner= GetOwner();
+	FRotator newRotator = FRotator(0, -60, 0);
+	Owner->SetActorRotation(newRotator);
+	
+	// ...
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
---
+++ BuildingEsacpe/Source/BuildingEsacpe/PositionReport.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BuildingEsacpe.h"
+#include "PositionReport.h"
+
+
+// Sets default values for this component's properties
+UPositionReport::UPositionReport()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	bWantsBeginPlay = true;
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UPositionReport::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	AActor* Actor = GetOwner();
+	FString ObjectName = Actor->GetName();
+	FString ObjectPos = Actor->GetTransform().GetLocation().ToString();
+	UE_LOG(LogTemp, Warning, TEXT("%s %s"), *ObjectName, *ObjectPos);
+}
+
+
+// Called every frame
+void UPositionReport::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+{
+	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
+
+	// ...
+}
+
